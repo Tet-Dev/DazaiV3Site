c6a0fc4e19ab8b4ef1c0652a183fd74d495982a4
John Li (Tet) <tet@tet.moe>
2023-05-03 00:02:17 -0700

    force clientID to auto set

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,7 @@
 export const discordAPI = "https://discord.com/api/v10";
 export const clientID =
-  process.env.NODE_ENV === "development"
+  process.env.CLIENT_ID ||
+  (process.env.NODE_ENV === "development"
     ? "876635342567006228"
-    : "755260934699745441";
+    : "755260934699745441");
 export const botOwnerID = "295391243318591490";
