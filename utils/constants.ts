1edd5b5f47813c20ba1ffa690ed0a34e95a771f1
9t8 <63334933+9t8@users.noreply.github.com>
2023-05-09 21:24:11 -0700

    rename

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,6 @@
 export const discordAPI = "https://discord.com/api/v10";
 export const clientID =
-  process.env.CLIENT_ID ||
+  process.env.NEXT_PUBLIC_CLIENT_ID ||
   (process.env.NODE_ENV === "development"
     ? "876635342567006228"
     : "755260934699745441");
